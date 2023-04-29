c76011d38d5b710229010e7d70c563d481335836
Erebus <Corado.Denkel@KoblenzCBS.onmicrosoft.com>
2023-04-29 14:22:06 +0200

    add Note class with properties

---
+++ src/Models/Note.java
@@ -0,0 +1,10 @@
+package Models;
+
+public class Note {
+    public float Value;
+    public double Gravity;
+    public Note(float value, double gravity) {
+        Value = value;
+        Gravity = gravity;
+    }
+}
