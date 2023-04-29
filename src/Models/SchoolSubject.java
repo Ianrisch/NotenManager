aac2a1243bd800c87e827527f7dcb573a113dc66
pants <unconfigured@null.spigotmc.org>
2023-04-29 14:52:42 +0200

    add SchoolSubject constructor

--- src/Models/SchoolSubject.java
+++ src/Models/SchoolSubject.java
@@ -3,8 +3,15 @@ package Models;
 import java.util.List;
 
 public class SchoolSubject {
-    public String className;
-    public String teacher;
+    public String name;
+    public Person teacher;
     public List<Grade> grades;
+
+    public SchoolSubject(Person teacher, String name, List<Grade> grades){
+        this.teacher=teacher;
+        this.name=name;
+        this.grades=grades;
+
+    }
 
 }
