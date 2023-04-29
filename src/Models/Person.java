c04a9394790f5bb59769f971ade33f602c80083e
pants <unconfigured@null.spigotmc.org>
2023-04-29 14:51:42 +0200

    add Person constructor

--- src/Models/Person.java
+++ src/Models/Person.java
@@ -3,4 +3,10 @@ package Models;
 public class Person {
     public String firstName;
     public String lastName;
+
+    public Person(String firstName, String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+
+    }
 }
