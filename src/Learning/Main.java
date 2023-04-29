9b964a2a8370bddd8e68180fceef0c20b6bf9bb9
ThunfischEis <ian.risch@gmail.com>
2023-04-29 13:31:12 +0200

    [No-Issue] Add Main Class for Learning Purposes

---
+++ src/Learning/Main.java
@@ -0,0 +1,8 @@
+package Learning;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(1, 1);
+        System.out.println(calculator.CalculateSum());
+    }
+}
