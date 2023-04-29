1c59c03a82e49cbdbd59b05617a5e394637d34e7
ThunfischEis <ian.risch@gmail.com>
2023-04-29 14:12:49 +0200

    Add Print for left calculator Number

--- src/Learning/Main.java
+++ src/Learning/Main.java
@@ -3,6 +3,7 @@ package Learning;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator(1, 1);
+        System.out.println("Calculator left value: " + calculator.LeftNumber);
         System.out.println(calculator.CalculateSum());
     }
 }
