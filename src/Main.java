fad357400e441a3c914ce715e26115a0449a8897
Erebus <Corado.Denkel@KoblenzCBS.onmicrosoft.com>
2023-04-29 16:31:40 +0200

    add AverageCalculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import Models.Grade;
+import Models.Person;
+import Models.SchoolSubject;
+import Utils.AverageCalculation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Grade> meineNoten = new ArrayList<Grade>();
+        Grade note1 = new Grade(6,1);
+        Grade note2 = new Grade(1,1);
+        meineNoten.add(note1);
+        meineNoten.add(note2);
+        Person meinTeacher = new Person("Tom", "Müller");
+        SchoolSubject meinSubject = new SchoolSubject(meinTeacher, "LF5", meineNoten);
+        System.out.println(AverageCalculation.average(meinSubject));
     }
 }
---
+++ src/Utils/AverageCalculation.java
@@ -0,0 +1,17 @@
+package Utils;
+
+import Models.Grade;
+import Models.SchoolSubject;
+
+public class AverageCalculation {
+    public static double average(SchoolSubject subject) {
+        double allGradesAdded = 0;
+        for(Grade grade : subject.grades) {
+            allGradesAdded += grade.Value;
+            System.out.println(allGradesAdded);
+        }
+        long gradeCount = subject.grades.size();
+        double average = allGradesAdded/gradeCount;
+        return average;
+    }
+}
